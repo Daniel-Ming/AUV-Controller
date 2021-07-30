a01e54eb449f90793e84c31a03b9347493c8fcae
daniel <z5179089@ad.unsw.edu.au>
2021-07-30 16:58:27 +0800

    content update

--- src/MPC_Controller.cpp
+++ src/MPC_Controller.cpp
@@ -18,7 +18,6 @@ int main(int argc, char** argv){
     ControlVector<control_dim> u_lb;
     ControlVector<control_dim> u_hb;
 
-//    x0 << 0,0,0,0,0,0,0,0;
     x_final << 0,0,0,0,0,0,0,0;
     u_lb << -1000,-1000,-1000,-1000,0,-10,-10, 1;
     u_hb << 1000, 1000, 1000, 1000,0,10, 10, 1;
@@ -144,13 +143,9 @@ int main(int argc, char** argv){
      */
     auto start_time = std::chrono::high_resolution_clock::now();
     // limit the maximum number of runs in this example
-    size_t maxNumRuns = 50;
+    size_t maxNumRuns = 11;
     std::cout << "Starting to run MPC" << std::endl;
     for (size_t i = 0; i < maxNumRuns; i++){
-        // let's for simplicity, assume that the "measured" state is the first state from the optimal trajectory plus some noise
-//        if (i > 0)
-//            x0 = 0.1 * StateVector<state_dim>::Random();
-
         // time which has passed since start of MPC
         auto current_time = std::chrono::high_resolution_clock::now();
         ct::core::Time t = 1e-6 * std::chrono::duration_cast<std::chrono::microseconds>(current_time - start_time).count();
@@ -175,82 +170,4 @@ int main(int argc, char** argv){
     }
     // the summary contains some statistical data about time delays, etc.
     ilqr_mpc.printMpcSummary();
-
-
-    /********* Put constraint on control input and Setting final desired state through constraint ***********/
-//    std::shared_ptr<ct::optcon::ConstraintContainerAnalytical<state_dim, control_dim>> inputConstraints(new ct::optcon::ConstraintContainerAnalytical<state_dim, control_dim>());
-//    std::shared_ptr<ControlInputConstraint<state_dim, control_dim>> controlInConstraint(new ControlInputConstraint<state_dim, control_dim>(u_lb, u_hb));
-//    controlInConstraint->setName("InputConstraint");
-//    inputConstraints->addIntermediateConstraint(controlInConstraint, true);
-//    inputConstraints->initialize();
-//
-//    optConProblem.setGeneralConstraints(inputConstraints);
-//
-//    // we include the desired terminal state as a hard constraint
-//    std::shared_ptr<ct::optcon::ConstraintContainerAnalytical<state_dim, control_dim>> finalConstraints(new ct::optcon::ConstraintContainerAnalytical<state_dim, control_dim>());
-//    std::shared_ptr<TerminalConstraint<state_dim, control_dim>> terminalConstraint(new TerminalConstraint<state_dim, control_dim>(x_final));
-//    terminalConstraint->setName("TerminalConstraint");
-//    finalConstraints->addTerminalConstraint(terminalConstraint, true);
-//    finalConstraints->initialize();
-//
-//    optConProblem.setInitialState(x0);
-//    optConProblem.setGeneralConstraints(finalConstraints);
-    /**********************************************************************************************************/
-
-    /*************** COnfigure ilqr settings and MPC settings ***********/
-//    NLOptConSettings ilqr_settings_mpc;
-//    ilqr_settings_mpc.dt = 0.01;  // the control discretization in [sec]
-//    ilqr_settings_mpc.integrator = ct::core::IntegrationType::EULERCT;
-//    ilqr_settings_mpc.discretization = NLOptConSettings::APPROXIMATION::FORWARD_EULER;
-//    ilqr_settings_mpc.max_iterations = 1;
-//    ilqr_settings_mpc.nlocp_algorithm = NLOptConSettings::NLOCP_ALGORITHM::GNMS;
-//    ilqr_settings_mpc.lqocp_solver = NLOptConSettings::LQOCP_SOLVER::GNRICCATI_SOLVER;  // the LQ-problems are solved using a custom Gauss-Newton Riccati solver
-//    ilqr_settings_mpc.printSummary = true;
-//
-//
-//    size_t K = ilqr_settings_mpc.computeK(timeHorizon); // K is the number of discrete time steps for an arbitrary input time interval
-//    FeedbackArray<state_dim, control_dim> u0_fb(K, FeedbackMatrix<state_dim, control_dim>::Zero());
-//    ControlVectorArray<control_dim> u0_ff(K, ControlVector<control_dim>::Zero());
-//    StateVectorArray<state_dim> x_ref_init(K + 1, x0);
-//    NLOptConSolver<state_dim, control_dim>::Policy_t initSolutionGuess(x_ref_init, u0_ff, u0_fb, ilqr_settings_mpc.dt);
-//
-//    ct::optcon::mpc_settings mpc_settings;
-//    mpc_settings.stateForwardIntegration_ = true;
-//    mpc_settings.postTruncation_ = true;
-//    mpc_settings.measureDelay_ = true;
-//    mpc_settings.delayMeasurementMultiplier_ = 1.0;
-//    mpc_settings.mpc_mode = ct::optcon::MPC_MODE::FIXED_FINAL_TIME;
-//    mpc_settings.coldStart_ = false;
-//    /************************************************************************************/
-//
-//    MPC<NLOptConSolver<state_dim, control_dim>> ilqr_mpc(optConProblem, ilqr_settings_mpc, mpc_settings);
-//    ilqr_mpc.setInitialGuess(initSolutionGuess);
-//
-//    auto start_time = std::chrono::high_resolution_clock::now();
-//    size_t maxNumRuns = 100;
-//    std::cout << "Starting to run MPC" << std::endl;
-//    for (size_t i = 0; i < maxNumRuns; i++){
-//        // time which has passed since start of MPC
-//        auto current_time = std::chrono::high_resolution_clock::now();
-//        ct::core::Time t = 1e-6 * std::chrono::duration_cast<std::chrono::microseconds>(current_time - start_time).count();
-//
-//        // prepare mpc iteration
-//        ilqr_mpc.prepareIteration(t);
-//        // new optimal policy
-//        ct::core::StateFeedbackController<state_dim, control_dim> newPolicy;
-//        // timestamp of the new optimal policy
-//        ct::core::Time ts_newPolicy;
-//
-//        current_time = std::chrono::high_resolution_clock::now();
-//        t = 1e-6 * std::chrono::duration_cast<std::chrono::microseconds>(current_time - start_time).count();
-//
-//        bool success = ilqr_mpc.finishIteration(x0, t, newPolicy, ts_newPolicy);
-//        newPolicy.computeControl(x0,t,u0);
-//        cout << success << endl;
-//
-//        // we break the loop in case the time horizon is reached or solve() failed
-//        if (ilqr_mpc.timeHorizonReached() | !success)
-//            break;
-//    }
-//    ilqr_mpc.printMpcSummary();
 }
